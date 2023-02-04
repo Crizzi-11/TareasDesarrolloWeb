709d9a36dc9d64038e1c7bef20e05a63d16f1590
Crizzi-11 <123390504+Crizzi-11@users.noreply.github.com>
2023-02-04 10:59:00 -0600

    Feat: Se agrego 3 paginas con base a la tarea
    Se realizaron 3 páginas con base a la tarea de aviones y con creatividad de cada uno

---
+++ Tarea02/src/main/java/com/Tarea03/demo/Tarea03Application.java
@@ -0,0 +1,13 @@
+package com.Tarea03.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea03Application.class, args);
+	}
+
+}

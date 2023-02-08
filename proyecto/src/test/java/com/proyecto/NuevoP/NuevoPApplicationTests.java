9c0aea1c097d564de891cfd71c6050c329851107
Yariella <107289578+yariella@users.noreply.github.com>
2023-02-07 22:18:54 -0600

    Feat: Tarea parte Yariella
    adjunto lo de las 2 paginas

---
+++ proyecto/src/test/java/com/proyecto/NuevoP/NuevoPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.NuevoP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuevoPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
